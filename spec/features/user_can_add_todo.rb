8538bd835892aace32f508370f2bfd3081d173d8
renagadex2 <etan.berkowitz@gmail.com>
2014-03-14 15:23:54 -0400

    capybara user submits todo added

---
+++ spec/features/user_can_add_todo.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe "a user can interact with a todo" do
+	it "lets user create a todo" do
+		
+		visit root_path
+		fill_in "New Todo", with: "do your grammar homework"
+
+		click_button "Submit"
+
+		find("#todo-list").should have_content("do your grammar homework") 
+
+	end
+
+
+end
