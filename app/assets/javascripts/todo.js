bb123e2db238966a7958b6effa6f37a8fc62b217
renagadex2 <etan.berkowitz@gmail.com>
2014-03-14 17:29:38 -0400

    checkboxes added

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -3,12 +3,20 @@ var todoFieldInput = $("input");
 $("form").submit(function(e) {
 	e.preventDefault();
 	$.post("/newtodo", { todo: todoFieldInput.val() })
+	$("<li>").text(todoFieldInput.val()).append('<input type="checkbox">').appendTo("ul");
 	this.reset();
 });
 
 $.getJSON("/new", function(response) {
-	console.log(response);
 	$.each(response, function(index, text) {
 		$("<li>").text(text.todo).appendTo("ul"); 
 	});
 });
+
+// $('input:checkbox').live('change', function(){
+//     if($(this).is(':checked')){
+//         alert('checked');
+//     } else {
+//         alert('un-checked');
+//     }
+// });
--- spec/features/user_can_add_todo.rb
+++ spec/features/user_can_add_todo.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe "a user can interact with a todo" do
-	it "lets user create a todo" do
+	it "lets user create a todo", javascript: true do
 		
 		visit root_path
 		fill_in "New Todo", with: "do your grammar homework"
 
 		click_button "Submit"
 
-		find("#todo-list").should have_content("do your grammar homework") 
+		page.should have_selector('li', text: "do your grammar homework")
 
 	end
 
 	# it "lets user delete a todo" do
 
-		
+
 
 	# end
 
